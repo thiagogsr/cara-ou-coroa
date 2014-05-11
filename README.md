b3bb039570a69dbf165a127c6c2296c86d415150
Thiago Guimaraes <thiagogsr@gmail.com>
2014-05-11 11:33:21 -0300

    Cálculos e gráficos implementados

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-cara-ou-coroa
+Cara ou Coroa
 =============
 
 Programa de probabilidade para o jogo de cara ou coroa
+
+### Requisitos:
+- Sinatra
+- HAML
+
+### Instalação
+1. Clone o projeto através da URL <https://github.com/thiagogsr/cara-ou-coroa.git>
+1. Entre na pasta charstats
+1. Instale a gem [Bundler](http://bundler.io) através do comando `gem install bundler`
+1. Instale as gems através do comando `bundle install`
+1. Inicie a aplicação com o comando `rackup -p 4567`
+
+## License
+MIT licensed
+
+Copyright (C) 2014 Thiago Guimarães, <http://twitter.com/thiagogsr>
--- app.rb
+++ app.rb
@@ -10,11 +10,17 @@ get '/' do
   haml :index
 end
 
-post '/run' do
-  @count = params[:count].to_i
-  @numbers = @count.times.map { Random.new.rand(0..1) }
-  @result = @numbers.each.map { |n| n == 1 ? "Cara" : "Coroa" }
-  @trueCount = @numbers.reduce(:+)
-  @falseCount = @numbers.length - @trueCount
+post '/' do
+  @count = params[:count]
+  @numbers = Array.new
+  @result = Array.new
+  @trueCount = Array.new
+  @falseCount = Array.new
+  @count.each_with_index do |c, i|
+    @numbers << c.to_i.times.map { Random.new.rand(0..1) }
+    @result << @numbers[i].to_a.each.map { |n| n == 1 ? "Cara" : "Coroa" }
+    @trueCount << @numbers[i].to_a.reduce(:+)
+    @falseCount << @numbers[i].to_a.length - @trueCount[i].to_i
+  end
   haml :result
 end

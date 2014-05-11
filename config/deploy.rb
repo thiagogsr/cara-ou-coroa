d22b4aa239ab5efcbc5826cee7b3ac95f6dc985a
Thiago Guimaraes <thiagogsr@gmail.com>
2014-05-11 14:58:34 -0300

    Mina deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -59,7 +59,7 @@ task :deploy => :environment do
     # Put things that will set up an empty directory into a fully set-up
     # instance of your project.
     invoke :'git:clone'
-    invoke :'deploy:link_shared_paths'
+    # invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
     #invoke :'rails:db_migrate'
     #invoke :'rails:assets_precompile'
